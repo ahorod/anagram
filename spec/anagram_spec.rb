4cf8c5b16b7b6ae6c1bf38d3083f8ac82ffaa150
ahorod <prozerpina112009@gmail.com>
2017-04-21 09:46:10 -0700

    add spec to check if words are anagram regardless of capitalization

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -1,12 +1,25 @@
 require('rspec')
 require('anagram')
 
+describe('String#anagram?') do
+  it('Check if two words are anagram') do
+    expect('eat, tea'.anagram?()).to(eq(true))
+  end
+  it('Check if two words are NOT anagram') do
+    expect('hello, love'.anagram?()).to(eq(false))
+  end
+  it('Check if two words are anagram regardless of capitalization') do
+    expect('Tea, Eat'.anagram?()).to(eq(true))
+  end
+  it('Check if two words are NOT anagram regardless of capitalization') do
+    expect('lOVe, HeLLo'.anagram?()).to(eq(false))
+  end
+end
 describe('String#palindrome?') do
   it('Check if a word is palindrome') do
-    expect('Level.'.palindrome?()).to(eq(true))
+    expect('Level'.palindrome?()).to(eq(true))
   end
   it('Check if a word is NOT a palindrome') do
     expect('testtesttest'.palindrome?()).to(eq(false))
   end
-
 end
