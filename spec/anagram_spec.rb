6477257cf49d3d2466aad09e40d790083d1cc140
ahorod <prozerpina112009@gmail.com>
2017-04-21 09:54:54 -0700

    add spec to check if two words are antigrams

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -15,19 +15,27 @@ describe('String#anagram?') do
     expect('lOVe, HeLLo'.anagram?()).to(eq(false))
   end
 end
-describe('String#word?') do
-  it('Check if two words are actual words') do
-    expect('listen, silent'.word?()).to(eq(true))
-  end 
-  it('Check if two words are NOT actual words') do
-    expect('yurb, ruby'.word?()).to(eq(false))
-  end
-end
 describe('String#palindrome?') do
   it('Check if a word is palindrome') do
     expect('Level'.palindrome?()).to(eq(true))
   end
   it('Check if a word is NOT a palindrome') do
     expect('testtesttest'.palindrome?()).to(eq(false))
+  end
+end
+describe('String#word?') do
+  it('Check if two words are actual words') do
+    expect('listen, silent'.word?()).to(eq(true))
+  end
+  it('Check if two words are NOT actual words') do
+    expect('yurb, ruby'.word?()).to(eq(false))
+  end
+end
+describe('String#antigram?') do
+  it('Check if two words are antigrams') do
+    expect('hi, bye'.word?()).to(eq(true))
+  end
+  it('Check if two words are NOT antigrams') do
+    expect('united, untied'.word?()).to(eq(false))
   end
 end
